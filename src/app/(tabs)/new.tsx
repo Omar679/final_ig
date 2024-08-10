d90e033def7add41d7924b0520daa718a09e1f91
Umar <omarsabiu679@gmail.com>
2024-08-10 11:48:10 +0100

    Image Picker

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,20 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Image, Pressable, Text, TextInput, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const defaultImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg";
 
 export default function CreatePost() {
   const [caption, setCaption] = useState("");
+  const [image, setImage] = useState(null || String);
+
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <View className="p-3 items-center flex-1 ">
       {/* Image Picker */}
       <Image
-        source={{ uri: defaultImage }}
+        source={{ uri: image ? image : defaultImage }}
         className="w-52 aspect-[3/4] rounded-lg shadow-md"
       />
 
-      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+      <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
         Change
       </Text>
       {/* Text Inpute */}
@@ -25,7 +47,10 @@ export default function CreatePost() {
         onChangeText={(value) => setCaption(value)}
       />
       {/* Submit Button */}
-      <Pressable style={{marginTop:'auto'}} className="bg-blue-500 p-4 items-center rounded-md w-full ">
+      <Pressable
+        style={{ marginTop: "auto" }}
+        className="bg-blue-500 p-4 items-center rounded-md w-full "
+      >
         <Text className="text-white font-semibold">Share</Text>
       </Pressable>
     </View>
