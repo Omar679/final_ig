771b36a3a652376b6841e544747c03401aeabd6d
Umar <omarsabiu679@gmail.com>
2024-08-22 15:34:28 +0100

    Push 1/2

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useAuth } from "~/src/providers/AuthProvider";
+import NotificationProvider from "~/src/providers/NotificationProvider";
 
 export default function TabsLayout() {
   const { isAuthenticated } = useAuth();
@@ -11,40 +12,42 @@ export default function TabsLayout() {
 
   return (
     <>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: "black",
-          tabBarShowLabel: false,
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            headerTitle: "For you",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name="home" size={26} color={color} />
-            ),
+      <NotificationProvider>
+        <Tabs
+          screenOptions={{
+            tabBarActiveTintColor: "black",
+            tabBarShowLabel: false,
           }}
-        />
-        <Tabs.Screen
-          name="new"
-          options={{
-            headerTitle: "Create Post",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name="plus-square-o" size={26} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            headerTitle: "Profile",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name="user" size={26} color={color} />
-            ),
-          }}
-        />
-      </Tabs>
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              headerTitle: "For you",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name="home" size={26} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="new"
+            options={{
+              headerTitle: "Create Post",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name="plus-square-o" size={26} color={color} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="profile"
+            options={{
+              headerTitle: "Profile",
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name="user" size={26} color={color} />
+              ),
+            }}
+          />
+        </Tabs>
+      </NotificationProvider>
     </>
   );
 }
